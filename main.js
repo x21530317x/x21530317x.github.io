814c655ead7ac9701e210214a260644f16e3d7ef
x21530317x <76546267+x21530317x@users.noreply.github.com>
2021-06-21 11:24:56 +0800

    Add files via upload
    update

---
+++ main.js
@@ -0,0 +1,12 @@
+//alert("歡迎來到比其堡");
+function reverse(s) {
+    return s.split("").reverse().join("");
+}
+function get_reverse() {
+    var content = document.getElementById('content').value;
+    re_content = reverse(content);
+    //    alert(re_content);
+    document.getElementById('result').innerHTML = "輸入的字串顛倒後為: " + re_content;
+}
+var sss = reverse("adam")
+console.log(sss)
